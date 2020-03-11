9dcf4228a86d67c89b8381126e4fd84635121fe2
taehyundev <K.anon.11107@gmail.com>
2020-03-11 22:19:50 +0900

    update 2020-03-11

---
+++ SQL/ORDER BY/ASC.sql
@@ -0,0 +1,6 @@
+/*
+오름차순 정렬
+tables라는 이름의 테이블에
+column을 기준으로 오름차순정렬
+*/
+select * from tables order by column ASC;
---
+++ SQL/ORDER BY/DESC.sql
@@ -0,0 +1,7 @@
+/*
+내림차순 정렬
+tables라는 이름의 테이블에
+column을 기준으로 내림차순정렬
+조건식 Where을 넣어야하는 경우 order by 앞에 넣으면 됨
+*/
+select * from tables order by column DESC;
